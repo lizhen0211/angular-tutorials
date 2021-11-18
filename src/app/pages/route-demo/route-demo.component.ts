51c6861555e2e10b5a71bd5f272c4d7c56c66e53
lizhen <lizhen@zhumengyuan.com>
2021-11-18 13:45:43 +0800

    add reuse stratety

--- src/app/pages/route-demo/route-demo.component.ts
+++ src/app/pages/route-demo/route-demo.component.ts
@@ -1,19 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, NavigationStart, Router} from "@angular/router";
 import {filter} from "rxjs/operators";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-route-demo',
   templateUrl: './route-demo.component.html',
   styleUrls: ['./route-demo.component.less']
 })
-export class RouteDemoComponent implements OnInit {
+export class RouteDemoComponent implements OnInit, OnDestroy {
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
   }
 
+  // @ts-ignore
+  private routerSubscription: Subscription;
+
   ngOnInit(): void {
-    this.router.events
+    this.routerSubscription = this.router.events
       .pipe(filter(e => e instanceof NavigationStart))
       .subscribe(res => {
         // @ts-ignore
@@ -23,6 +27,10 @@ export class RouteDemoComponent implements OnInit {
           this.activatedRoute.snapshot.data.iskeep = false;
         }
       });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription.unsubscribe();
   }
 
   public onGoClick(): void {
